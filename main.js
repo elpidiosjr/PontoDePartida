6c4ec0cc709e0913b2e2e36fffc0de04fceb57b3
Elpidio <elpidiosoares@gmail.com>
2022-05-18 17:42:06 -0300

    alterando o projeto

---
+++ main.js
@@ -0,0 +1,10 @@
+function onScroll() {
+  navigation.classList.add('scroll')
+
+    if(scrollY > 0) {
+        navigation.classList('scroll')
+    }else {
+        navigation.classList.remove('scroll')
+    }
+
+}
